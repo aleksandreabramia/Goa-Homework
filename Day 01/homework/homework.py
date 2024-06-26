c790734030bff06ea8aa366f17402e5541b083a4
aleksandreabramia <magdaandguladze77@gmail.com>
2024-06-26 19:54:44 +0400

    second commit

---
+++ Day 01/homework/homework.py
@@ -0,0 +1,16 @@
+name= "aleksandre"
+
+surename= "abramia"
+
+age= 15
+
+height= 170
+
+no_cap="ar icis_programireba"
+real ="aris_chadi_da_moggeri" 
+
+place= "Tbilisshi"
+yes="unda_rom_iswavlos_programireba"
+
+
+print(name+" "+ surename + "da aris" + " " + str(age )+ " wlis " + " igi aris aseve" + " " + str(height)+ "cm" "." "cxovrobs"+ " " +place + "."+  no_cap+ " " +yes + " "  + real)
